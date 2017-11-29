233d6c4642896f961d2c0491aad153a84575fdd9
uderos <ubehome@gmail.com>
2017-11-28 21:33:10 -0500

    W3 WIP

--- w3-hw.py
+++ w3-hw.py
@@ -31,6 +31,9 @@ qb111 = tensor(qb1, qb1, qb1)
 qbHp = (1.0 / math.sqrt(2)) * (qb0 + qb1)
 qbHm = (1.0 / math.sqrt(2)) * (qb0 - qb1)
 
+qbY0 = (1.0 / math.sqrt(2)) * (qb0 + 1j * qb1)
+qbY1 = (1.0 / math.sqrt(2)) * (qb0 - 1j * qb1)
+
 
 def normalizer(N):
 	return 1.0 / math.sqrt(N)
@@ -62,8 +65,40 @@ def calc_Hmin(rhoX):
 	return Hmin
 
 
+def calc_pGuess(psiA, plusKet, minusKet):
+	rhoA = ket2dm(psiA)
+	rhoPlus = ket2dm(plusKet)
+	rhoMinus = ket2dm(minusKet)
+	pPlus = (rhoA * rhoPlus).tr().real
+	pMinus = (rhoA * rhoMinus).tr().real
+	pGuess = max(pPlus, pMinus)
+	print("p+=%g p-=%g pGuess=%g" % (pPlus, pMinus, pGuess))
+	return pGuess
+
+	
+
+def problem_3_2():
+	print("== PAGE 3 PROBLEM 2 ==")
+	alpha = 1.0 / math.sqrt(2 + math.sqrt(2))
+	psiA = alpha * (qb0 + qbHp)
+	psiPlus1 = qb0
+	psiMinus1 = qb1
+	psiPlus2 = qbHp
+	psiMinus2 = qbHm
+	psiPlus3 = qbY0
+	psiMinus3 = qbY1
+	pGuess1 = calc_pGuess(psiA, psiPlus1, psiMinus1)
+	pGuess2 = calc_pGuess(psiA, psiPlus2, psiMinus2)
+	pGuess3 = calc_pGuess(psiA, psiPlus3, psiMinus3)
+	pGuess = (pGuess1 + pGuess2 + pGuess3) / 3.0
+	print("pGuess = %r" % pGuess)
+
+
+	
+
+
 def problem_1_2():
-	print("== PAGE 1 PROBLEM 2 ==")
+	print("== PAGE 3 PROBLEM 2 ==")
 
 	rhoX = ket2dm(qb00)
 	calc_Hmin(rhoX)
@@ -94,6 +129,7 @@ def main():
 	#test02()
 	#test03()
 	problem_1_2()
+	problem_3_2()
 
 if __name__ == "__main__":
 	main()
